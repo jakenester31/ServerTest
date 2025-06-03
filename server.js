1a15b59e5e4b800ead9eead3ed7605f070d1d0d5
jakenester31 <111662560+jakenester31@users.noreply.github.com>
2025-06-02 21:41:19 -0700

    commit

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 document.querySelector('#box').innerHTML = 1;
+const xhttp = new XMLHttpRequest();
 
 function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("demo").innerHTML = this.responseText;
+    document.querySelector('#demo').innerHTML = val.test.c[0];
+}
+
+// get using ajax
+xhttp.open('GET','data.JSON',true);
+xhttp.send();
+
+// set using ajax
+xhttp.open('POST','data.JSON',true);
+xhttp.setRequestHeader('Content-Type','application/json');
+xhttp.send(JSON.stringify({a:'test1'}));
+
+xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200){
+        val = JSON.parse(this.response);
+        console.log(val);
     }
-  };
-  xhttp.open("GET", "ajax_info.txt", true);
-  xhttp.send();
 }
